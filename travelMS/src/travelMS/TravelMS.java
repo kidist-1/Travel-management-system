f1715373171ba74e74599e5db5a22a9643a0cdae
shahy <shahy@DESKTOP-NELADPE>
2018-06-02 22:22:06 +0530

    TravelMS basic structure

---
+++ travelMS/src/travelMS/TravelMS.java
@@ -0,0 +1,24 @@
+package travelMS;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shahy
+ */
+public class TravelMS 
+{
+    public void display_packages()
+    {
+        int cost,choice;
+        System.out.println("******Welcome to travelopedia******");
+        System.out.println("Enter your destination of travel");
+        /*Create a switch case into do while and enter any 3 destinations of ypur choice...and every case should
+        have the total cost and various addons delivered */
+                
+        
+    }
+}
