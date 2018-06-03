50ee582957f5ffff5d9b5aeaa927f214504edc4f
SHREE SHANTERI <SHREE SHANTERI@SHREESHANTERI>
2018-06-03 13:01:19 +0530

    initial commit

--- travelMS/src/travelMS/TravelMS.java
+++ travelMS/src/travelMS/TravelMS.java
@@ -1,24 +1,32 @@
 package travelMS;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author shahy
- */
+import java.util.Scanner;
+
 public class TravelMS 
 {
-    public void display_packages()
-    {
-        int cost,choice;
-        System.out.println("******Welcome to travelopedia******");
-        System.out.println("Enter your destination of travel");
-        /*Create a switch case into do while and enter any 3 destinations of ypur choice...and every case should
-        have the total cost and various addons delivered */
-                
-        
+      public static void main(String[] args) {
+       
+      Scanner inp=new Scanner(System.in);
+      PackageDetails P = new PackageDetails();
+      
+      exit:
+      while(true)
+      {
+          System.out.println("Enter 1.View tour package details. 2.Book a package. 3.Cancel a package. "
+                  + "4.View transaction details. 5.Exit");
+          int ch=inp.nextInt();
+          
+          switch(ch)
+          {
+              case 1: {
+                        P.details();
+                        P.travelMode();
+                        break;
+                       }
+              
+              case 5: break exit;
+          }
+      }
     }
-}
+
+}   
