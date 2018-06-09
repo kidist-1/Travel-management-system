94db963139c24351f71eea2f6ebacfc94e7da364
User <User@Suhasini>
2018-06-10 00:38:41 +0530

    travelMS class modified.

--- travelMS/src/travelMS/TravelMS.java
+++ travelMS/src/travelMS/TravelMS.java
@@ -8,7 +8,6 @@ public class TravelMS
       {
          int i,count;
          double GST_cal = 0;
-         double customerDetails = 0;
          double total_package=0,rate=0,GST=0;  
         BookPackage[] B= new BookPackage[10];
         for(i=0; i<B.length; i++)
@@ -29,7 +28,7 @@ public class TravelMS
         exit:
       while(true)
       {
-          System.out.println("Enter 1.View tour package details. 2.Book a package. 3Cancel a package 4.Exit");
+          System.out.println("Enter 1.View tour package details. 2.Book a package. 3.Exit");
           choice=inp.nextInt();
           switch(choice)
           {
@@ -39,24 +38,18 @@ public class TravelMS
                         break;
                        }
                case 2:{
-                        System.out.println("Enter the number of people who would like to book the Package:");
+                        System.out.println("Enter the number of packages you want to book(only one at a time)");
                         count=inp.nextInt();
                         for(i=0;i<count;i++)
                         {
-                            customerDetails= B[i].customerDetails(total_package);
-                            GST_cal = bill[i].GST_calculate(B[i].customerDetails(total_package),rate,GST);
+                            total_package=B[i].customerDetails(total_package);
+                            GST_cal = bill[i].GST_calculate(total_package,rate,GST);
                             System.out.println("The GST amount = "+GST_cal);
                         };
                         break;
                         
                      }
-               
-               case 3:{
-                         double total_amt = C.cancelDetails(customerDetails); 
-                         System.out.println("Total amount = "+total_amt);
-                         break;
-                      }
-               case 4: break exit;
+               case 3: break exit;
                
               default:System.out.println("Invalid choice entered!!!!");
               
