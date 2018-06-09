86e2954e22cb5a9d32ea8799689e1c566771cab0
User <User@Suhasini>
2018-06-09 21:20:34 +0530

    book package and billing classes modified.

--- travelMS/src/travelMS/Billing.java
+++ travelMS/src/travelMS/Billing.java
@@ -9,17 +9,17 @@ import java.text.DecimalFormat;
  *
  * @author shahy
  */
-public class Billing extends BookPackage
+public class Billing
 {
     private static DecimalFormat df2 = new DecimalFormat(".##");
     
     public double GST_calculate(double total_package,double rate,double GST)
     {
-        System.out.println("The total_amt:: " +df2.format(total_package));
+        System.out.println("The total_amt = " +df2.format(total_package));
         
         GST=(total_package*12)/112;
         rate=total_package-GST;
-        System.out.println("The Rate::" +df2.format(rate));
+        System.out.println("The Rate = " +df2.format(rate));
         
         return GST;
     }
--- travelMS/src/travelMS/TravelMS.java
+++ travelMS/src/travelMS/TravelMS.java
@@ -6,7 +6,9 @@ public class TravelMS
 {
       public static void main(String[] args)
       {
-            int i,count;
+         int i,count;
+         double GST_cal = 0;
+         double customerDetails = 0;
          double total_package=0,rate=0,GST=0;  
         BookPackage[] B= new BookPackage[10];
         for(i=0; i<B.length; i++)
@@ -21,11 +23,13 @@ public class TravelMS
         }
         Scanner inp=new Scanner(System.in);
         PackageDetails P = new PackageDetails();
+        CancelPackage C=new CancelPackage();
         int choice;
      
-      do
+        exit:
+      while(true)
       {
-          System.out.println("Enter 1.View tour package details. 2.Book a package. 3.Exit");
+          System.out.println("Enter 1.View tour package details. 2.Book a package. 3Cancel a package 4.Exit");
           choice=inp.nextInt();
           switch(choice)
           {
@@ -39,16 +43,24 @@ public class TravelMS
                         count=inp.nextInt();
                         for(i=0;i<count;i++)
                         {
-                            double GST_cal=bill[i].GST_calculate(B[i].customerDetails(total_package),rate,GST);
-                            System.out.println("The GST charged will be::"+GST_cal);
-                        }
-                        
+                            customerDetails= B[i].customerDetails(total_package);
+                            GST_cal = bill[i].GST_calculate(B[i].customerDetails(total_package),rate,GST);
+                            System.out.println("The GST amount = "+GST_cal);
+                        };
                         break;
+                        
                      }
+               
+               case 3:{
+                         double total_amt = C.cancelDetails(customerDetails); 
+                         System.out.println("Total amount = "+total_amt);
+                         break;
+                      }
+               case 4: break exit;
+               
               default:System.out.println("Invalid choice entered!!!!");
               
           }
-      }while(choice!=3);
+      }
     }
-
 }   
