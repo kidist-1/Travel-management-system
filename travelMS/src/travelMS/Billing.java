608ededae51e595fcdbe98ac5d9d1bb997051c18
SHREE SHANTERI <SHREE SHANTERI@SHREESHANTERI>
2018-06-09 11:32:29 +0530

    decimal precision added

--- travelMS/src/travelMS/Billing.java
+++ travelMS/src/travelMS/Billing.java
@@ -12,12 +12,13 @@ import java.text.DecimalFormat;
 public class Billing extends BookPackage
 {
     private static DecimalFormat df2 = new DecimalFormat(".##");
+    
     public double GST_calculate(double total_package,double rate,double GST)
     {
         System.out.println("The total_amt:: " +df2.format(total_package));
         
         rate=(total_package*12)/112;
-        System.out.println("The Rate::"+rate);
+        System.out.println("The Rate::" +df2.format(rate));
         GST=total_package-rate;
         return GST;
     }
